171e926d47f73786d5daef380bde3a716d185611
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-14 16:34:58 +0530

    added dart_basics_doc file

---
+++ dart_basics/lib/dart_basic_doc.dart
@@ -0,0 +1,113 @@
+// addNumbers(num1,num2){
+//   print(num1+num2);
+// }
+// void main() {
+//   addNumbers(2,2);
+//   print('Hello!');
+// }
+
+
+
+// addNumbers(int num1,int num2){
+//   print(num1+num2);
+// }
+// void main() {
+//   addNumbers(2,2.4); //error
+//   print('Hello!');
+// }
+
+
+
+// addNumbers(double num1,double num2){
+//   print(num1+num2);
+// }
+// void main() {
+//   addNumbers(2.3,2.4); 
+//   print(addNumbers(2.3,2.4));
+//   print('Hello!');
+// }
+
+
+
+
+// void addNumbers(double num1,double num2){
+//   return num1 + num2;
+// }
+// void main() {
+//   print(addNumbers(2.3,2.4)); //error
+//   print('Hello!');
+// }
+
+
+
+// int addNumbers(int num1,int num2){
+//   return num1 + num2;
+// }
+// void main() {
+//   print(addNumbers(2,2));
+
+//   print('Hello!');
+// }
+
+
+// ***************   varibales    *************
+
+// int addNumbers(int num1,int num2){
+//   return num1 + num2;
+// }
+// void main() {
+//   var demo=addNumbers(2,2);
+//   demo=addNumbers(23,1);
+//   print(demo + 1);
+//   print('Hello!');
+// }
+
+
+// double addNumbers(double num1,double num2){
+//   return num1 + num2;
+// }
+// void main() {
+//   double demo;
+//   demo=addNumbers(23,1);
+//   print(demo + 1);
+//   print('Hello!');
+// }
+
+// ******** class and objects **********
+
+// class Person{
+//     String name="max";
+//     int age=30;
+// }
+// void main() {  
+//   //creating an instance of class person
+//   var p1=Person();
+//   print(p1); //Instance of 'Person'
+//   print(p1.name); //max
+//   print(p1.age); //30 
+//   var p2=Person();
+//   print(p2); //Instance of 'Person'
+//   p2.name='min';
+//   p2.age=40;
+//   print(p2.name); //min
+//   print(p2.age); //40
+// }
+
+
+// *******  Constructor    *******
+// class Person{
+//     String name;
+//     int age;
+  
+//   Person(String myName,int age){
+//     name = myName;
+//     this.age=age;
+//   }
+// }
+// void main() {  
+//   var p1=Person('Shaily',23);
+//   print(p1.name); //Shaily
+//   print(p1.age); //23
+// }
+
+
