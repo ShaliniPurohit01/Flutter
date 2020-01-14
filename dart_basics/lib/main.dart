13dbe7f0dc26d4e019f9a6597d684ce07da11887
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-14 18:39:52 +0530

    annonymous functon

--- dart_basics/lib/main.dart
+++ dart_basics/lib/main.dart
@@ -18,8 +18,11 @@ class MyApp extends StatelessWidget {
       body: Column(children: /*<Widget>*/[
         Text('The Question!'),
         RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
-        RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
-        RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!'),),
+        RaisedButton(child: Text('Answer 3'), onPressed: (){
+           print('Answer 3 chosen');
+          },
+        ),
 
       ],),
     ),);
