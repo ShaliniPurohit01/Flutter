48b3589f37862ad99c4d5228be9503ca51eed87e
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-14 18:28:14 +0530

    connecting functions and buttons

--- dart_basics/lib/main.dart
+++ dart_basics/lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
 void main() =>   runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!!'); 
+  }
   @override
   Widget build(BuildContext context) {
     var questions=[
@@ -18,9 +17,9 @@ class MyApp extends StatelessWidget {
       appBar:AppBar(title:Text('Welcome to Quiz'),),
       body: Column(children: /*<Widget>*/[
         Text('The Question!'),
-        RaisedButton(child: Text('Answer 1'), onPressed: null,),
-        RaisedButton(child: Text('Answer 2'), onPressed: null,),
-        RaisedButton(child: Text('Answer 3'), onPressed: null,),
+        RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
 
       ],),
     ),);
