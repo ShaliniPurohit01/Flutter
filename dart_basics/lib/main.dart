3a7559e8e307924afd3712f2b85fba9832d0a83c
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-14 18:14:05 +0530

    adding layout widget

--- dart_basics/lib/main.dart
+++ dart_basics/lib/main.dart
@@ -10,9 +10,19 @@ void main() =>   runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions=[
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp( home:Scaffold(
       appBar:AppBar(title:Text('Welcome to Quiz'),),
-      body: Text('this is my quiz body !!'),
+      body: Column(children: /*<Widget>*/[
+        Text('The Question!'),
+        RaisedButton(child: Text('Answer 1'), onPressed: null,),
+        RaisedButton(child: Text('Answer 2'), onPressed: null,),
+        RaisedButton(child: Text('Answer 3'), onPressed: null,),
+
+      ],),
     ),);
   }
 }
