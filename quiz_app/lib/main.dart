a7bef915bc6bbc3ae43e8f50dabbc0d37893c730
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-15 07:46:28 +0530

    quiz app initial commit

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() =>   runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!!'); 
+  }
+  @override
+  Widget build(BuildContext context) {
+    var questions=[
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp( home:Scaffold(
+      appBar:AppBar(title:Text('Welcome to Quiz'),),
+      body: Column(children: /*<Widget>*/[
+        Text('The Question!'),
+        RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!'),),
+        RaisedButton(child: Text('Answer 3'), onPressed: (){
+           print('Answer 3 chosen');
+          },
+        ),
+
+      ],),
+    ),);
+  }
+}
