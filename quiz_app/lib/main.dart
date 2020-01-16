2c3f5b7ce29d140d93ddf62b8e72f1b9881f18ff
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-16 13:53:28 +0530

    constants

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,69 +2,76 @@ import 'package:flutter/material.dart';
 import './questions.dart';
 import './answers.dart';
 
-void main() =>   runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return _MyAppState();
   }
 }
 
-class _MyAppState extends State<MyApp>
-{
-  var _questionIndex=0;
-  void _answerQuestion(){
-    setState(
-      (){
-        _questionIndex=_questionIndex+1;
-        }
-    );
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
     print(_questionIndex);
   }
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     //maps {key:value}
-    var questions=[
+    //this is compile time constant
+    const questions = [
       {
-        'questionText':'What\'s your favorite color?',
-        'answer':['Black','Red','Blue','Green'],
+        'questionText': 'What\'s your favorite color?',
+        'answer': ['Black', 'Red', 'Blue', 'Green'],
       },
       {
-        'questionText':'What\'s your favorite animal?',
-        'answer':['Horse','Dog','Cow','Cat'],
+        'questionText': 'What\'s your favorite animal?',
+        'answer': ['Horse', 'Dog', 'Cow', 'Cat'],
       },
       {
-        'questionText':'Which\'s your food?',
-        'answer':['Indian','Chinese','Continental','Italian'],
+        'questionText': 'Which\'s your food?',
+        'answer': ['Indian', 'Chinese', 'Continental', 'Italian'],
       }
     ];
+
+    // var dummy = ['hello'];
+    // dummy.add('Max!!');
+    // print(dummy);
+    // [hello, Max!!]
+
+    // var dummy =const ['hello'];
+    // dummy.add('Max!!');
+    // print(dummy); //error
+    //we add new value to unmodifiedable variable
+    // dummy=[]; // this is allowed because dummy is variable but list is constant
+
     return MaterialApp(
-      home:Scaffold(
-        appBar:AppBar(
-          title:Text('Welcome to Quiz'),
-        ),
-        body: Column(
-          children:[
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-          //callbacks
-          // Answer(_answerQuestion),
-          // Answer(_answerQuestion),
-          // Answer(_answerQuestion),
-          
-          //mapping list to widget
-            ...(
-              questions
-              [_questionIndex]['answer'] as List<String>
-            ).map((answers){
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to Quiz'),
+          ),
+          body: Column(
+            children: [
+              Question(
+                questions[_questionIndex]['questionText'],
+              ),
+              //callbacks
+              // Answer(_answerQuestion),
+              // Answer(_answerQuestion),
+              // Answer(_answerQuestion),
+
+              //mapping list to widget
+              ...(questions[_questionIndex]['answer'] as List<String>)
+                  .map((answers) {
                 return Answer(_answerQuestion, answers);
               }).toList()
-          ],
-        )
-      ),
+            ],
+          )),
     );
   }
 }
