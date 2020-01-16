43ea304c71e120621ea9cf2c25ee68bbecba8387
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-16 10:39:13 +0530

    callback as answer

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,45 +1,48 @@
 import 'package:flutter/material.dart';
 import './questions.dart';
+import './answers.dart';
 
 void main() =>   runApp(MyApp());
 
 class MyApp extends StatefulWidget{
-
   @override
   State<StatefulWidget> createState(){
     return _MyAppState();
   }
 }
 
-class _MyAppState extends State<MyApp> {
-
+class _MyAppState extends State<MyApp>
+{
   var _questionIndex=0;
-
   void _answerQuestion(){
-    setState((){
-    _questionIndex=_questionIndex+1;
-
-    });
+    setState(
+      (){
+        _questionIndex=_questionIndex+1;
+        }
+    );
     print(_questionIndex);
   }
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     var questions=[
       'What\'s your favorite color?',
       'What\'s your favorite animal?',
     ];
-    return MaterialApp( home:Scaffold(
-      appBar:AppBar(title:Text('Welcome to Quiz'),),
-      body: Column(children: /*<Widget>*/[
-        Question(questions[_questionIndex],),
-        RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion,),
-        RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!'),),
-        RaisedButton(child: Text('Answer 3'), onPressed: (){
-           print('Answer 3 chosen');
-          },
-        ),
-
-      ],),
-    ),);
+    return MaterialApp(
+       home:Scaffold(
+        appBar:AppBar(title:Text('Welcome to Quiz'),
+      ),
+      body: Column(
+        children:[
+          Question(
+            questions[_questionIndex],
+          ),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+        ],
+      ),
+    );
   }
 }
