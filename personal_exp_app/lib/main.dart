a7df5798c3707908d206438525f083cf8abaf135
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-18 08:20:46 +0530

    styling card  in pers_exp

--- personal_exp_app/lib/main.dart
+++ personal_exp_app/lib/main.dart
@@ -52,15 +52,39 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.deepPurpleAccent,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.deepPurpleAccent,
+                        ),
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tx.title),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         Text(
                           tx.date.toString(),
+                          style: TextStyle(color: Colors.blueGrey),
                         ),
                       ],
                     ),
