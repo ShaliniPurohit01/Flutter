09eb6a3f533bca1bc342e3f7b95032bd6b1d6096
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-18 09:09:57 +0530

    importing external package for date format in pers_exp

--- personal_exp_app/lib/main.dart
+++ personal_exp_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -64,7 +65,8 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        // tx.amount.toString(),
+                        ' Rs. ${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -83,7 +85,8 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          // tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: TextStyle(color: Colors.blueGrey),
                         ),
                       ],
