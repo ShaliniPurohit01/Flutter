4764d2f3262c229894e4c0190c260249035782ab
Shalini Purohit <purohit.shaily001@gmail.com>
2020-01-20 13:45:26 +0530

    splitting app into widgets pers_exp app

---
+++ personal_exp_app/lib/widgets/new_transaction.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountControlller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              // onChanged: (val) {
+              //   amountInput = val;
+              // },
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.deepPurpleAccent,
+              onPressed: () {
+                print(titleController.text);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
