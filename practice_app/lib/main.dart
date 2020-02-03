4a0799117c4fc948dd84eb7718ede2c4bd69b084
Shalini Purohit <purohit.shaily001@gmail.com>
2020-02-03 23:07:01 +0530

    login page ui  in radio app

--- practice_app/lib/main.dart
+++ practice_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import './screen/radio_list_screen.dart';
-import 'package:radio/screen/login.dart';
+import 'package:practice_app/screens/practice_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Radio App',
+      title: 'Practice App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: Practice(),
     );
   }
 }
---
+++ practice_app/lib/screens/practice_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Practice extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practice app'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Demo Text'),
+        ),
+      ),
+    );
+  }
+}
